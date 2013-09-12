bd7d7d7e397b46e041b1ead8d6d0fe0849b0d3d8
Jacob Foster <jacob@britecore.com>
2013-09-12 18:53:51 -0500

    Laying down project structure.

---
+++ src/interface/Main.java
@@ -0,0 +1,10 @@
+package interface;
+
+public class Main
+{
+    // This is is the file that will run it's main method
+    public static void main(String [] args)
+    {
+
+    }
+}
---
+++ src/objects/Class.java
@@ -0,0 +1,7 @@
+package objects;
+
+// This class encapsulates logic for the instructor's Classes
+public class Class
+{
+
+}
