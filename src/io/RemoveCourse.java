2e1344042692dfaf013d684c0dd075a2f55953b0
weddle715 <weddle715@gmail.com>
2013-10-20 18:08:45 -0500

    Created the RemoveCourse.java file

---
+++ src/io/RemoveCourse.java
@@ -0,0 +1,13 @@
+/**
+ * "Deletes" a class from preloading by moving the XML file to an
+ * archive folder
+ *
+ * @author Chris Weddle
+ * @version 10-20-13
+ */
+
+import java.io.File;
+
+public static void main (String argv []) {
+    
+}
