350507ebe5b05407fa5bae4cb3f2cc8b7ab89bca
unknown <garbageelitist@gmail.com>
2013-10-29 12:24:12 -0500

    Student is archived when removed and then turned into a ghost student. parseXML now works with new way of storing grades.

---
+++ gradebooks/archive/records/README.md
@@ -0,0 +1,2 @@
+This folder must exist currently for archiveStudent to work, README exists to create blank directory.
+In the future, archiveStudent will work without filepath created.
--- src/objects/Student.java
+++ src/objects/Student.java
@@ -1,5 +1,12 @@
 package objects;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 /**
  * Class Student models a students first, last, and full name as well
  * as holding a pseudo name that can not be altered after being assigned
@@ -80,9 +87,65 @@ public class Student {
     /**
      * Returns the pseudo name assigned to the student
      * 
-     * @return		Pseudo name8
+     * @return		Pseudo name
      */
     public String getPseudoName() {
         return pseudoName;
     }
+    
+    /**
+     * Places student into in a text file found in "archive/records/StudentName-CourseIDCourseNumber-Semeste.txtr"
+     */
+    public void archiveStudent(MyCourse course) {
+    	String name = getLastName() + getFirstName();
+    	String courseInfo = '-' +  course.getCourseID() + course.getCourseNumber() + '-' + course.getSemester();
+    	name = name.replaceAll("\\s+", "");
+    	courseInfo = courseInfo.replaceAll("\\s+", "");
+    	
+    	String filePath = "gradebooks" + System.getProperty("file.separator") + "archive" + System.getProperty("file.separator") + 
+    						"records" + System.getProperty("file.separator");
+
+    	//FIXME mkdir doesn't work, directory must exist before file creation
+    	File path = new File(filePath);
+    	path.mkdir();
+    	
+    	filePath += name + courseInfo;
+    	
+    	File file = new File(filePath + ".txt");
+    	
+    	//FIXME should add current date to end of file instead of random number
+    	if (file.exists()) {
+    		int random = (int)(Math.random() * 1000 + 1);
+    		filePath += "-" + random;
+    		file = new File(filePath + ".txt");
+    	}
+    	
+    	try {
+    		file.createNewFile();
+    		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+    		
+    		writer.write(getFullName() + " - " + course.getName() + System.getProperty("line.separator"));
+    		
+    		for (int i = 0; i < course.getNumberOfAssignmentCategories(); i++) {
+    			AssignmentCategory asnCat = course.getAssignmentCategory(i);
+    			writer.write(System.getProperty("line.separator") + asnCat.getName() + System.getProperty("line.separator"));
+    			
+    			for (int j = 0; j < asnCat.getNumberOfAssignments(); j++) {
+    				Assignment asn = asnCat.getAssignment(j);
+    				writer.write(asn.getName() + ": " + asn.getGrade(getPseudoName()) + 
+    					"/" + asn.getWorth() + System.getProperty("line.separator"));
+    			}
+    		}
+    		
+    		writer.close();
+    	} catch (IOException e) {
+    		System.out.println(e);
+    	}
+    }
+    
+    /**
+     * Takes all of the student's information and converts it to a GhostStudent
+     * 
+     * @return		GhostStudent object
+     */
 }
