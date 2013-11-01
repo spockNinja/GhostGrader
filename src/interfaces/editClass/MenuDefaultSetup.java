8ebb1f2fb0b8879a7ce415aa402324e78219ec29
LilongHuang <lilonghuang@yahoo.com>
2013-11-01 12:14:18 -0500

    rename interface to interfaces

---
+++ src/interfaces/editClass/MenuDefaultSetup.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GhostGradeBook.editClass;
+
+/**
+ *
+ * @author Lilong
+ */
+public class MenuDefaultSetup {
+    
+}
