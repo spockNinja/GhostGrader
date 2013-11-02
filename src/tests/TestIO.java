4472c22199355ce4d578e252c2b44f5e73c731fa
Jacob Foster <jacob@britecore.com>
2013-11-02 15:39:52 -0500

    Updating IO test file to remove exported files unless told otherwise.

--- src/tests/TestIO.java
+++ src/tests/TestIO.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.io.File;
 import io.*;
 
 public class TestIO {
@@ -8,8 +9,28 @@ public class TestIO {
         TestHelper t = new TestHelper();
         Exporter exp = new Exporter();
 
+        String[] filePaths = new String[t.courses.length];
+
         for (int i=0; i< t.courses.length; i++) {
-            exp.exportCourseToHTML(t.courses[i], "testExport" + i + ".html");
+            filePaths[i] = "testExport" + i + ".html";
+            exp.exportCourseToHTML(t.courses[i], filePaths[i]);
+        }
+
+        if (args.length > 0 && args[0].equals("keepFiles")) {
+            for (int j=0; j<filePaths.length; j++) {
+                System.out.println("Exported " + filePaths[j]);
+            }
+        }
+        else {
+            for (int j=0; j<filePaths.length; j++) {
+                try {
+                    File f = new File(filePaths[j]);
+                    f.delete();
+                }
+                catch (Exception e) {
+                    System.err.println("Failed to delete " + filePaths[j]);
+                }
+            }
         }
     }
 
